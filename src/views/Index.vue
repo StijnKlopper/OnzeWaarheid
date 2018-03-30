57f705cf6fe6cad62d9ca5978d30da0d0f96d66e
StijnKlopper <33661926+StijnKlopper@users.noreply.github.com>
2018-03-30 23:42:36 +0200

    added image (lennyface)
    (lennyface)

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,21 @@
 <template>
   <v-container>
-    Onze Waarheid
+    <v-layout
+      row
+      wrap
+      justify-center>
+      <v-flex xs8 >
+        <v-jumbotron
+          color = "white"
+          class = "jumbo">
+          <img
+            src="@/assets/logo.png"
+            alt="Logo"
+            width="898px"
+            height="126px">
+        </v-jumbotron>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
@@ -11,5 +26,4 @@ export default {
 </script>
 
 <style>
-
 </style>
